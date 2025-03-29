cf511334104f757c12f5bd02457b908763f3860b
YolandaM16 <mookie.mokwena@gmail.com>
2025-03-29 15:08:41 +0200

    created views

--- Library_Catalog/views.py
+++ Library_Catalog/views.py
@@ -1,3 +1,67 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
+from rest_framework import status
+from rest_framework import ListModelMixin, CreateModelMixin, UpdateModelMixin, DestroyModelMixin
+from rest_framework.response import Response
+from rest_framework.viewsets import ModelViewSet
+from rest_framework.generics import ListCreateAPIView, RetrieveUpdateDestroyAPIView
+from .models import Book, Author, CustomUser, Transaction
+from .serializers import BookSerializer, AuthorSerializer, CustomUserSerializer, TransactionSerializer
 
 # Create your views here.
+class BookList(ListCreateAPIView):
+    def get_queryset(self):
+        return Book.object.all()
+    
+    def get_serializer_class(self):
+        return BookSerializer
+    
+
+class BookDetail(RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        return Book.objects.all()
+    
+    def get_serializer(self):
+        return BookSerializer
+    
+
+class AuthorList(ListCreateAPIView):
+    def get_queryset(self):
+        return Author.object.all()
+    
+    def get_serializer_class(self):
+        return AuthorSerializer
+    
+class AuthorDetail(RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        return Author.object.all()
+    
+    def get_serializer_class(self):
+        return AuthorSerializer
+    
+class CustomUserList(ListCreateAPIView):
+    def get_queryset(self):
+        return CustomUser.object.all()
+    
+    def get_serializer_class(self):
+        return CustomUserSerializer
+    
+class CustomUserDetail(RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        return CustomUser.object.all()
+    
+    def get_serializer_class(self):
+        return CustomUserSerializer
+    
+class TransactionList(ListCreateAPIView):
+    def get_queryset(self):
+        return Transaction.object.all()
+    
+    def get_serializer_class(self):
+        return TransactionSerializer
+    
+class TransactionDetail(RetrieveUpdateDestroyAPIView):
+    def get_queryset(self):
+        return Transaction.object.all()
+    
+    def get_serializer_class(self):
+        return TransactionSerializer
